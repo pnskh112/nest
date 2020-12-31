e1fc6def4908efaa831c3ef54a25ef1043408eb3
pnskh112 <pnskh112pnskh112pnskh112@gmail.com>
2020-12-31 21:56:54 +0900

    upd:tasks.controller.ts

--- src/tasks/task.repository.ts
+++ src/tasks/task.repository.ts
@@ -2,6 +2,7 @@ import { Entity, EntityRepository, Repository } from "typeorm";
 import { Task } from "./task.entity";
 import { CreateTaskDto } from './dto/create-task.dto';
 import { TaskStatus } from './task-status.enum';
+import { NotFoundException, Delete } from '@nestjs/common';
 
 @EntityRepository(Task)
 export class TaskRepository extends Repository<Task> {
@@ -15,4 +16,16 @@ export class TaskRepository extends Repository<Task> {
 
         return task;
     }
+
+    deleteTask(id: number): void {
+        const task = new Task();
+        const found = this.findOne(id);
+        if(!found) {
+            throw new NotFoundException(`Task with ID "${id}" not found`);
+        }
+        // found.delete();
+        // return this.taskRepository.deleteTask(id);
+        // return found;
+    }
 }
+

9f3d02bbeed144866a5798179f60d822c33b0380
pnskh112 <pnskh112pnskh112pnskh112@gmail.com>
2021-01-01 23:25:37 +0900

    upd:auth.controller.ts

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+import { AuthCredentialsDto } from './dto/auth.credential.dto';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(
+        private authService: AuthService,
+    ) {}
+
+    @Post('/signUp')
+    signUp(@Body() authCredentialsDto: AuthCredentialsDto): Promise<void>{
+        return this.authService.signUp(authCredentialsDto);
+    }
+}
